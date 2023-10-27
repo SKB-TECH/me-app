d94e5896bc2a07c930a58f64ba6400d9635c3a39
Benjamin <shakokinyamba201@gmail.com>
2023-10-27 17:48:02 -0400

    First commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      he
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/canvas/Stars.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stars = () => {
+  return (
+    <div>
+      start canavas
+    </div>
+  )
+}
+
+export default Stars
