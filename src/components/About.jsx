60e4b00d498819110424174a5dda79357168162c
Benjamin <shakokinyamba201@gmail.com>
2023-10-27 20:38:21 -0400

    Feat: create component Hero

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,17 @@
+import Tilt from 'react-tilt';
+import {motion} from 'framer-motion';
+import {styles} from '../style';
+import { services } from '../constantes';
+import {fadeIn,textVariant} from '../utils/motion';
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+    </>
   )
 }
 
