ede66475b53f43762437f0e841551bf2e3797a28
Benjamin <shakokinyamba201@gmail.com>
2023-10-28 05:09:13 -0400

    component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+
 import { styles } from "../style";
 import { ComputersCanvas } from "./canvas";
 
@@ -15,33 +16,33 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Benjamin</span>
+            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm web develop, user <br className='sm:block hidden' />
-            interfaces and mobile applications
+            I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces and web applications
           </p>
         </div>
       </div>
-      <ComputersCanvas />
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-                <motion.div
-                  animate={{
-                    y:[0,24,0]
-                  }}
-                  transition={{
-                    duration:1.5,
-                    repeat:Infinity,
-                    repeatType:'loop'
-                  }}
 
-                  className="w-3 h-3 rounded-full bg-secondary mb-1"
-                />
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
           </div>
         </a>
-
       </div>
     </section>
   );
