05b541595a35e4f8f9c8aa5f9690edf767f53df7
Benjamin <shakokinyamba201@gmail.com>
2023-10-29 21:42:53 -0400

    Feat: create component contact

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -8,7 +8,7 @@ const FeedBackCard=({index,testimonial,name,designation,company,image})=>{
     return (
         <motion.div 
             variants={fadeIn("","spring",index*0.5,0.75)}
-            className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+            className="bg-black-200 p-10 rounded-3xl xs:w-[300px] w-full"
         >
             <p className="text-white font-black text-[48px]">"</p>
             <div className="mt-1">
